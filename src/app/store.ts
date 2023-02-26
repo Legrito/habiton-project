0b4412c869268c2ede50bc21e07a96003e1c0f58
Lesya <jamma@i.ua>
2023-02-26 21:12:07 +0100

    Add middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+
+const loggerMiddleware: Middleware = storeAPI => next => action => {
+  console.log('dispatching', action);
+  console.log('next state', storeAPI.getState());
+  return next(action);
+}
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(loggerMiddleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
