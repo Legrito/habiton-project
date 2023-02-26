8e35b301f66cfa6b0bdd0e347a3ddbe7be0b28f7
Lesya <jamma@i.ua>
2023-02-26 21:12:54 +0100

    Add nameAddad reducer

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,10 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
+  name: string;
   value: number;
 }
 
 const initialState: CounterState = {
+  name: '',
   value: 0
 }
 
@@ -12,6 +14,9 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
+    nameAdded(state, action: PayloadAction<string>) {
+      state.name = action.payload;
+    },
     incremented(state) {
       // we can "mutate" the state, because immer is under 
       // the hood and it deal with that
